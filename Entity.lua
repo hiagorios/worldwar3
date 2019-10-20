6130d225c9ce4d1092e54f3526af288399a038c8
Hiago Rios <hiagoriosc@gmail.com>
2019-10-20 12:09:27 -0300

    Generic

--- Entity.lua
+++ Entity.lua
@@ -1,13 +1,21 @@
 Entity = Object:extend()
 
-function Entity:new(x, y)
-    
+function Entity:new(x, y, img, direction)
+    self.x = x
+    self.y = y
+    self.img = img
+    self.quadro = 0
+    self.direction = direction
+    self.vel_x = 0
+    self.vel_y = 0
+    self.acel_y = 0
+    self.acel_x = 0
 end
 
-function Entity:update()
+function Entity:update(dt)
 
 end
 
 function Entity:draw()
-
+    love.graphics.draw()
 end
